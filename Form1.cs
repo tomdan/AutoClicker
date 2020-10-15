370072ba3d861b1b272d50ec780a1aa5245d5d74
tomdan94 <tomdan94@gmail.com>
2020-10-15 15:06:18 +0200

    added mouse buttons

--- Form1.cs
+++ Form1.cs
@@ -23,7 +23,13 @@ namespace AutoClicker
         //to using mouse click
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
-        
+
+        // mouse buttons
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
         public AutoClickerForm()
         {
             InitializeComponent();
