6307a5681832e50a0847c59c5db28d150a0cc8f1
tomdan94 <tomdan94@gmail.com>
2020-12-15 14:39:48 +0100

    changed general form design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,13 +35,15 @@
             this.auto_clicker_timer = new System.Windows.Forms.Timer(this.components);
             this.points_to_click_listBox = new System.Windows.Forms.ListBox();
             this.clear_button = new System.Windows.Forms.Button();
+            this.panel_top = new System.Windows.Forms.Panel();
+            this.panel_top.SuspendLayout();
             this.SuspendLayout();
             // 
             // start_button
             // 
-            this.start_button.Location = new System.Drawing.Point(12, 12);
+            this.start_button.Location = new System.Drawing.Point(0, 0);
             this.start_button.Name = "start_button";
-            this.start_button.Size = new System.Drawing.Size(75, 23);
+            this.start_button.Size = new System.Drawing.Size(80, 40);
             this.start_button.TabIndex = 0;
             this.start_button.Text = "Start";
             this.start_button.UseVisualStyleBackColor = true;
@@ -56,9 +58,9 @@
             // cursor_coordinates_label
             // 
             this.cursor_coordinates_label.AutoSize = true;
-            this.cursor_coordinates_label.Location = new System.Drawing.Point(12, 38);
+            this.cursor_coordinates_label.Location = new System.Drawing.Point(35, 255);
             this.cursor_coordinates_label.Name = "cursor_coordinates_label";
-            this.cursor_coordinates_label.Size = new System.Drawing.Size(174, 19);
+            this.cursor_coordinates_label.Size = new System.Drawing.Size(181, 19);
             this.cursor_coordinates_label.TabIndex = 1;
             this.cursor_coordinates_label.Text = "Współrzędne kursora: 0 , 0";
             // 
@@ -69,39 +71,56 @@
             // 
             // points_to_click_listBox
             // 
+            this.points_to_click_listBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.points_to_click_listBox.Dock = System.Windows.Forms.DockStyle.Right;
             this.points_to_click_listBox.FormattingEnabled = true;
             this.points_to_click_listBox.ItemHeight = 19;
-            this.points_to_click_listBox.Location = new System.Drawing.Point(12, 60);
+            this.points_to_click_listBox.Location = new System.Drawing.Point(657, 0);
             this.points_to_click_listBox.Name = "points_to_click_listBox";
-            this.points_to_click_listBox.Size = new System.Drawing.Size(120, 194);
+            this.points_to_click_listBox.ScrollAlwaysVisible = true;
+            this.points_to_click_listBox.Size = new System.Drawing.Size(100, 461);
             this.points_to_click_listBox.TabIndex = 2;
             // 
             // clear_button
             // 
             this.clear_button.Font = new System.Drawing.Font("Times New Roman", 12F);
-            this.clear_button.Location = new System.Drawing.Point(93, 12);
+            this.clear_button.Location = new System.Drawing.Point(86, 0);
             this.clear_button.Name = "clear_button";
-            this.clear_button.Size = new System.Drawing.Size(75, 23);
+            this.clear_button.Size = new System.Drawing.Size(80, 40);
             this.clear_button.TabIndex = 3;
             this.clear_button.Text = "Wyczyść";
             this.clear_button.UseVisualStyleBackColor = true;
             this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
             // 
+            // panel_top
+            // 
+            this.panel_top.Controls.Add(this.start_button);
+            this.panel_top.Controls.Add(this.clear_button);
+            this.panel_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_top.Location = new System.Drawing.Point(0, 0);
+            this.panel_top.Name = "panel_top";
+            this.panel_top.Size = new System.Drawing.Size(657, 40);
+            this.panel_top.TabIndex = 4;
+            // 
             // AutoClickerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.clear_button);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(757, 461);
+            this.Controls.Add(this.panel_top);
             this.Controls.Add(this.points_to_click_listBox);
             this.Controls.Add(this.cursor_coordinates_label);
-            this.Controls.Add(this.start_button);
-            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(160)))), ((int)(((byte)(60)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AutoClickerForm";
+            this.Opacity = 0.8D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Auto Clicker";
             this.TopMost = true;
+            this.panel_top.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,6 +134,7 @@
         private System.Windows.Forms.Timer auto_clicker_timer;
         private System.Windows.Forms.ListBox points_to_click_listBox;
         private System.Windows.Forms.Button clear_button;
+        private System.Windows.Forms.Panel panel_top;
     }
 }
 
